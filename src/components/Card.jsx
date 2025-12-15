8473493de60dcf4b44537f9867c0d97bfc97288b
Nguyen Huy <huy246302@gmail.com>
2025-12-15 10:27:06 +0700

    tailwind-fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Card({ card, flipCard, isFlipped }) {
   return (
-    <div onClick={flipCard}>
+    <div 
+      className="
+          border border-indigo-100
+          p-4 mb-3 cursor-pointer rounded
+          hover:bg-indigo-50
+          hover:shadow-md
+          transition-all duration-200
+        "    onClick={flipCard}
+    >
       <div>
         {isFlipped ? card.answer : card.question}
       </div>
